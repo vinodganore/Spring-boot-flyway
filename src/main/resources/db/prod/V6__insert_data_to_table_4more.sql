a884e34003ad217896bd76f70c6653415d5361b8
vinodganore <vinodganore@gmail.com>
2020-04-10 06:33:46 +0530

    (feature)[FLYWAY] Project has been ran with profiles and inserted new data in prod env..

---
+++ src/main/resources/db/prod/V6__insert_data_to_table_4more.sql
@@ -0,0 +1,11 @@
+INSERT INTO REGISTRATION_USER(username,firstname,lastname,mobile,email) values
+('Narasimha','rao','anumula','254657855','a.rao@gmail.com');
+
+INSERT INTO REGISTRATION_USER(username,firstname,lastname,mobile,email) values
+('sampath','sampth','thotapally','5465784578','sam.totapally@gmail.com');
+
+INSERT INTO REGISTRATION_USER(username,firstname,lastname,mobile,email) values
+('kanak','goldking','gundeboina','65457845254','kanak.g@gmail.com');
+
+INSERT INTO REGISTRATION_USER(username,firstname,lastname,mobile,email) values
+('hareesh','hareesh','gajjelli','8754214578','g.hareesh@gmail.com');
